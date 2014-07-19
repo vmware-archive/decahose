25fab2cf90350dc6c5f5d06d902d14c40c69406b
Ronert Obst <ronert.obst@gmail.com>
2014-07-19 18:45:30 +0200

    flattened json to extract all field

--- sql/parsetweetjson.sql
+++ sql/parsetweetjson.sql
@@ -74,41 +74,52 @@
 -- 4) Define PL/Python function to parse the JSON
 ----------------------------------------------------------------------------------------
 
-	drop function if exists twitter.gnip_json_parse(text) cascade;
-	create or replace function twitter.gnip_json_parse(jsonblob text)
-	        returns twitter.gnip_tweet_columns
-	as
-	$$
-	    import json
-	    keys = [
-		    u'body', 
-			u'retweetCount', 
-			u'generator', 
-			u'twitter_filter_level', 
-			u'gnip', 
-			u'favoritesCount', 
-			u'object', 
-			u'actor', 
-			u'twitter_lang', 
-			u'twitter_entities', 
-			u'verb', 
-			u'link', 
-			u'provider', 
-			u'postedTime', 
-			u'id', 
-			u'objectType'
-		]
-	    jsonblob_cleaned = jsonblob.strip().replace('\r\n','').replace('\n','') if jsonblob else jsonblob
-	    jobj = None 
-	    try:
-	        jobj = json.loads(jsonblob_cleaned)
-	    except ValueError, e:
-	        jobj = None
-	    result = []
-	    for k in keys:
-	        result.append(jobj[k] if jobj and jobj.has_key(k) else None)
-	    return result
-	$$ language plpythonu;
+drop function if exists twitter.gnip_json_parse(text) cascade;
+create or replace function twitter.gnip_json_parse(jsonblob text)
+    returns twitter_ronert.gnip_tweet_columns
+    as
+    $$
+    import collections
+    import json
+
+    def flatten(d, parent_key='', sep='_'):
+        items = []
+        for k, v in d.items():
+            new_key = parent_key + sep + k if parent_key else k
+            if isinstance(v, collections.MutableMapping):
+                items.extend(flatten(v, new_key).items())
+            else:
+                items.append((new_key, v))
+        return dict(items)
+    keys = [
+        u'body',
+        u'retweetCount',
+        u'generator',
+        u'twitter_filter_level',
+        u'gnip',
+        u'favoritesCount',
+        u'object',
+        u'actor',
+        u'twitter_lang',
+        u'twitter_entities',
+        u'verb',
+        u'link',
+        u'provider',
+        u'postedTime',
+        u'id',
+        u'objectType'
+        ]
+    jsonblob_cleaned = jsonblob.strip().replace('\r\n','').replace('\n','') if jsonblob else jsonblob
+    jobj = None
+    try:
+        jobj = flatten(json.loads(jsonblob_cleaned))
+    except ValueError, e:
+        jobj = None
+    result = []
+    for k in keys:
+        result.append(jobj[k] if jobj and jobj.has_key(k) else None)
+    return result
+    $$ language plpythonu;
 
 ----------------------------------------------------------------------------------------
 -- 5) Invoke PL/Python function to parse the JSON from the tweets table
