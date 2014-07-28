f4497b55b4f35e5bbaec61c284f4e20a7d4d6eb0
Ronert Obst <ronert.obst@gmail.com>
2014-07-28 22:57:35 +0200

    actor parsing working now, stupid indent error

--- sql/parseactorsjson.sql
+++ sql/parseactorsjson.sql
@@ -7,110 +7,66 @@
     ----------------------------------------------------------------------------------------
 
 
-    preferredUsername text
-    displayName text
-    links_href text
-    links_rel text
-    twitterTimeZone text
-    image text
-    verified boolean
-    location_displayName text
-    location_objectType text
-    statusesCount int
-    summary text
-    languages text
-    utcOffset int
-    link text
-    followersCount int
-    favoritesCount int
-    friendsCount int
-    listedCount int
-    postedTime timestamp with time zone
-    id text
-    objectType text
-
-    preferredUsername
-    displayName
-    links_href
-    links_rel
-    twitterTimeZone
-    image
-    verified
-    location_displayName
-    location_objectType
-    statusesCount
-    summary
-    languages
-    utcOffset
-    link
-    followersCount
-    favoritesCount
-    friendsCount
-    listedCount
-    postedTime
-    id
-    objectType
-
 
     ----------------------------------------------------------------------------------------
 -- 2) Create a table to hold the results of the parsed fields from the JSON blobs
     ----------------------------------------------------------------------------------------
 
-drop table if exists twitter.tweets cascade;
+drop table if exists twitter.actors cascade;
 create table twitter.actors
     (
-        preferredUsername text
-        displayName text
-        links_href text
-        links_rel text
-        twitterTimeZone text
-        image text
-        verified boolean
-        location_displayName text
-        location_objectType text
-        statusesCount int
-        summary text
-        languages text
-        utcOffset int
-        link text
-        followersCount int
-        favoritesCount int
-        friendsCount int
-        listedCount int
-        postedTime timestamp with time zone
-        id text
+        preferredUsername text,
+        displayName text,
+        links_href text,
+        links_rel text,
+        twitterTimeZone text,
+        image text,
+        verified boolean,
+        location_displayName text,
+        location_objectType text,
+        statusesCount int,
+        summary text,
+        languages text,
+        utcOffset int,
+        link text,
+        followersCount int,
+        favoritesCount int,
+        friendsCount int,
+        listedCount int,
+        postedTime timestamp with time zone,
+        id text,
         objectType text
         )
-    distributed by (postedTime);
+    distributed by (id);
 
     ----------------------------------------------------------------------------------------
 -- 3) Define User Defined Composite Type to hold result of parsed JSON
     ----------------------------------------------------------------------------------------
 
-drop type if exists twitter.gnip_tweet_columns cascade;
-create type twitter.gnip_actors_columns
+drop type if exists twitter.gnip_actor_columns cascade;
+create type twitter.gnip_actor_columns
     as
     (
-        preferredUsername text
-        displayName text
-        links_href text
-        links_rel text
-        twitterTimeZone text
-        image text
-        verified boolean
-        location_displayName text
-        location_objectType text
-        statusesCount int
-        summary text
-        languages text
-        utcOffset int
-        link text
-        followersCount int
-        favoritesCount int
-        friendsCount int
-        listedCount int
-        postedTime timestamp with time zone
-        id text
+        preferredUsername text,
+        displayName text,
+        links_href text,
+        links_rel text,
+        twitterTimeZone text,
+        image text,
+        verified boolean,
+        location_displayName text,
+        location_objectType text,
+        statusesCount int,
+        summary text,
+        languages text,
+        utcOffset int,
+        link text,
+        followersCount int,
+        favoritesCount int,
+        friendsCount int,
+        listedCount int,
+        postedTime timestamp with time zone,
+        id text,
         objectType text
         );
 
@@ -118,15 +74,13 @@ create type twitter.gnip_actors_columns
 -- 4) Define PL/Python function to parse the JSON
     ----------------------------------------------------------------------------------------
 
-drop function if exists twitter.gnip_json_parse(text) cascade;
+drop function if exists twitter.gnip_json_parse_actors(text) cascade;
 create or replace function twitter.gnip_json_parse_actors(jsonblob text)
-    returns twitter.gnip_actors_columns
+    returns twitter.gnip_actor_columns
     as
     $$
 import collections
 import json
-
-
 def flatten(d, parent_key='', sep='_'):
     items = []
     for k, v in d.items():
@@ -135,40 +89,43 @@ def flatten(d, parent_key='', sep='_'):
             items.extend(flatten(v, new_key).items())
         else:
             items.append((new_key, v))
-            return dict(items)
-    keys = [
-        'actors_preferredUsername'
-        'actors_displayName'
-        'actors_links_href'
-        'actors_links_rel'
-        'actors_twitterTimeZone'
-        'actors_image'
-        'actors_verified'
-        'actors_location_displayName'
-        'actors_location_objectType'
-        'actors_statusesCount'
-        'actors_summary'
-        'actors_languages'
-        'actors_utcOffset'
-        'actors_link'
-        'actors_followersCount'
-        'actors_favoritesCount'
-        'actors_friendsCount'
-        'actors_listedCount'
-        'actors_postedTime'
-        'actors_id'
-        'actors_objectType'
-    ]
-    jsonblob_cleaned = jsonblob.strip().replace('\r\n','').replace('\n','') if jsonblob else jsonblob
+    return dict(items)
+keys = [
+    'actor_preferredUsername',
+    'actor_displayName',
+    'actor_links_href',
+    'actor_links_rel',
+    'actor_twitterTimeZone',
+    'actor_image',
+    'actor_verified',
+    'actor_location_displayName',
+    'actor_location_objectType',
+    'actor_statusesCount',
+    'actor_summary',
+    'actor_languages',
+    'actor_utcOffset',
+    'actor_link',
+    'actor_followersCount',
+    'actor_favoritesCount',
+    'actor_friendsCount',
+    'actor_listedCount',
+    'actor_postedTime',
+    'actor_id',
+    'actor_objectType'
+]
+jsonblob_cleaned = jsonblob.strip().replace('\r\n','').replace('\n','') if jsonblob else jsonblob
+jobj = None
+try:
+    jobj = flatten(json.loads(jsonblob_cleaned))
+except ValueError:
     jobj = None
-    try:
-        jobj = flatten(json.loads(jsonblob_cleaned))
-    except ValueError:
-        jobj = None
-    result = []
-    for k in keys:
-        result.append(jobj[k] if jobj and k in jobj else None)
-    return result
+result = []
+plpy.notice(jsonblob_cleaned)
+plpy.notice(jobj)
+for k in keys:
+    result.append(jobj[k] if jobj and k in jobj else None)
+plpy.notice(result)
+return result
     $$ language plpythonu;
 
     ----------------------------------------------------------------------------------------
