48f2b282f92af78f8701207c78118063cbe8a155
Ronert Obst <ronert.obst@gmail.com>
2014-08-29 18:01:07 +0100

    changed to PXF

--- sql/2_fetch_from_ext_table.sql
+++ sql/2_fetch_from_ext_table.sql
@@ -1,32 +1,56 @@
 ------------------------------------------------------------------------------------------------------
--- Fetch JSON blobs from HDFS through External Tables & PXF. Parse the data and insert
--- into HAWQ internal table.
+-- Fetch JSON blobs from HDFS through External Tables & PXF.
 -- Maintained by: Srivatsan Ramanujam <sramanujam@gopivotal.com>, Ronert Obst<robst@gopivotal.com>
 ------------------------------------------------------------------------------------------------------
 
 
 ----------------------------------------------------------------------------------------
--- 1) Define external HDFS table refercing the JSON stream
+-- 1) Define external HDFS table refercing the JSON stream with PXF Parser
 ----------------------------------------------------------------------------------------
 
-    drop external table if exists twitter.decahose_rawjson_ext cascade;
-    create external table twitter.decahose_rawjson_ext
+drop external table if exists twitter.decahose_rawjson_ext cascade;
+create external table twitter.decahose_rawjson_ext
     (
-        tweet_json text
-    )
-    LOCATION ('pxf://hdm1.gphd.local:50070/user/vatsan/decahose/{YEAR}/{MONTH}/{DAY}/*?profile=HdfsTextSimple')
-    FORMAT 'TEXT' (ESCAPE 'OFF');
+	"body" text,
+	"retweetCount" int,
+	"generator" text,
+	"twitter_filter_level" text,
+	"gnip" text,
+	"favoritesCount" int,
+	"object" text,
+	"twitter_lang" text,
+	"twitter_entities" text,
+	"verb" text,
+	"link" text,
+	"provider" text,
+	"postedTime" text,
+	"id" text,
+	"objectType" text,
+	"actor.preferredUsername" text,
+	"actor.displayName" text,
+	"actor.links" text,
+	"actor.twitterTimeZone" text,
+	"actor.image" text,
+	"actor.verified" boolean,
+	"actor.location.displayName" text,
+	"actor.location.objectType" text,
+	"actor.statusesCount" int,
+	"actor.summary" text,
+	"actor.languages" text,
+	"actor.utcOffset" int,
+	"actor.link" text,
+	"actor.followersCount" int,
+	"actor.favoritesCount" int,
+	"actor.friendsCount" int,
+	"actor.listedCount" int,
+	"actor.postedTime" text,
+	"actor.id" text,
+	"actor.objectType" text			
+        )
+    LOCATION ('pxf://hdm1.gphd.local:50070/user/vatsan/decahose/2014/08/27/*.txt?PROFILE=JSON')
+    FORMAT 'CUSTOM'  (FORMATTER='pxfwritable_import');
 
-----------------------------------------------------------------------------------------
--- 2) Invoke PL/Python functions to parse the JSON from the tweets table
-----------------------------------------------------------------------------------------
-
-    insert into twitter.tweets_master
-    select (cols).*
-    from
-    (
-        select twitter.gnip_json_parse(tweet_json) as cols
-        from twitter.decahose_rawjson_ext
-    )q;
-
-----------------------------------------------------------------------------------------
+drop table if exists twitter.master;
+create table twitter.master as
+select * from twitter.decahose_rawjson_ext
+distributed by (id) ;
